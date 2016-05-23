73410cf41173e70dbff859d52997b3dcda07e65d
a4rn <reynicolas@yahoo.com>
2016-05-23 00:51:50 -0400

    game updates

--- controllers/cats_controller.js
+++ controllers/cats_controller.js
@@ -7,66 +7,84 @@ var router = express.Router();
 var Cat = require('../models/models.js')[0];
 var User = require('../models/models.js')[1];
 
-router.get('/', function(req,res) {
-	res.redirect('/cats')
+router.get('/', function(req, res) {
+  res.redirect('/bitcoin')
 });
 
-router.get('/cats', function(req,res) {
-	Cat.findAll({}).then(function(result){
-		var hbsObject = {
-			cats : result,
-			logged_in : req.session.logged_in,
-			username : req.session.username
-		}
-		res.render('cats/index', hbsObject);
-	})
+router.get('/bitcoin', function(req, res) {
+  Cat.findAll({}).then(function(result) {
+    var hbsObject = {
+      cats: result,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    }
+    res.render('cats/index', hbsObject);
+  })
 });
 
-router.post('/cats/create', function(req,res) {
-	Cat.create({
-		name: req.body.name,
-		sleepy: req.body.sleepy,
-		user_id: req.session.user_id
-	}).then(function(newlyCreatedCat){
-		res.redirect('/cats')
-	});
-});
+router.post('/bitcoin/game/:id', function(req, res) {
+  var id = req.params.id;
+  console.log(req.body)
+  console.log('id = ' + id)
 
-router.put('/cats/update/:id', function(req,res) {
-	Cat.update({
-		sleepy: req.body.sleepy
-  },
-  {
-		where: { id : req.params.id }
-	}
-	).then(function (result) {
-		res.redirect('/cats');
-  }, function(rejectedPromiseError){
-		console.log(rejectedPromiseError);
+  burger.update(id, function(data) {
+    res.redirect('/index');
   });
 });
 
-router.delete('/cats/delete/:id', function(req,res) {
-	if (req.session.logged_in){
-		//check if cat belongs to user then let user delete cat
-		Cat.findAll({
-	    where: {$and: [{user_id: req.session.user_id}, {id: req.params.id}]}
-	  }).then(function(cats) {
-			if (cats.length > 0){
-				Cat.destroy({
-					where: { id : req.params.id }
-				}).then(function (result) {
-					res.redirect('/cats');
-				}, function(rejectedPromiseError){
-					console.log(rejectedPromiseError);
-				});
-			}else{
-				res.send("sorry you can't delete that cat.");
-			}
-		});
-	}else {
-		res.send("sorry you can't do that. You need to be logged in");
-	}
+router.post('/cats/create', function(req, res) {
+  Cat.create({
+    name: req.body.name,
+    sleepy: req.body.sleepy,
+    user_id: req.session.user_id
+  }).then(function(newlyCreatedCat) {
+    res.redirect('/cats')
+  });
+});
+
+router.put('/cats/update/:id', function(req, res) {
+  Cat.update({
+    sleepy: req.body.sleepy
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function(result) {
+    res.redirect('/cats');
+  }, function(rejectedPromiseError) {
+    console.log(rejectedPromiseError);
+  });
+});
+
+router.delete('/cats/delete/:id', function(req, res) {
+  if (req.session.logged_in) {
+    //check if cat belongs to user then let user delete cat
+    Cat.findAll({
+      where: {
+        $and: [{
+          user_id: req.session.user_id
+        }, {
+          id: req.params.id
+        }]
+      }
+    }).then(function(cats) {
+      if (cats.length > 0) {
+        Cat.destroy({
+          where: {
+            id: req.params.id
+          }
+        }).then(function(result) {
+          res.redirect('/cats');
+        }, function(rejectedPromiseError) {
+          console.log(rejectedPromiseError);
+        });
+      } else {
+        res.send("sorry you can't delete that cat.");
+      }
+    });
+  } else {
+    res.send("sorry you can't do that. You need to be logged in");
+  }
 
 });
 
