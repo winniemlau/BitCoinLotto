35104d358a87db5bb2fa01c0896cc3d0d22dd2b6
Bruno Marques <brunomarques@Brunos-MacBook-Pro.local>
2016-05-24 18:49:21 -0400

    schema/routing/otherSTUFF

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 ### Schema
-CREATE TABLE IF NOT EXISTS users_bitcoin
+CREATE TABLE users_bitcoin
 (
 	id int NOT NULL AUTO_INCREMENT,
 	username varchar(255) NOT NULL,
 	email varchar(255) NOT NULL,
 	password_hash varchar(255) NOT NULL,
 	PRIMARY KEY (id), 
-	balance int NOT NULL, 
+	balance  int DEFAULT 500, 
 	created_at DATETIME NOT NULL, 
 	updated_at DATETIME NOT NULL
 );
---
+++ public/assets/js/app.js
@@ -0,0 +1,25 @@
+// signup/signin 
+
+$('#login').on("click", function(){  
+    //stores 
+	var loginStuff= {
+		username: $("username").val().trim(), 
+		email:$("email").val().trim(), 
+		password:$("password").val().trim()
+	}
+	//grabs
+	$("username").val().trim(); 
+	$("email").val().trim(); 
+	$("password").val().trim(); 
+	
+	//clears all 
+	$("username").val("") 
+
+	$.ajax({
+		type:"POST",
+		url:"users/login", 
+		data: loginStuff 
+
+	}) 
+
+});
