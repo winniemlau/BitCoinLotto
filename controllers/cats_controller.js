caf0a390f472fba61d7b7e65c517d78a0c7ef30f
brotherother <dennis.r.flynn@gmail.com>
2016-05-28 09:54:28 -0400

    reys late fixes

--- controllers/cats_controller.js
+++ controllers/cats_controller.js
@@ -3,80 +3,92 @@ Here is where you create all the functions that will do the routing for your app
 */
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 var Cat = require('../models/models.js')[0];
 var User = require('../models/models.js')[1];
 
-router.get('/', function(req,res) {
-	res.redirect('/users/leaderboard')
+router.get('/', function(req, res) {
+  res.redirect('/users/leaderboard')
 });
 
-router.get('/cats/game', function(req,res) {
-	console.log(req.session);
-	if(req.session.logged_in) {
-		res.render('cats/game', {logged_in: req.session.logged_in})
-	} else {
-		res.redirect('/')
-	}
-	
+
+router.get('/cats/game', function(req, res) {
+  // console.log(req.session);
+  if (req.session.logged_in) {
+    res.render('game/game', {
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    })
+  } else {
+    res.redirect('/')
+  }
+
 });
 
-router.get('/cats', function(req,res) {
-	Cat.findAll({}).then(function(result){
-		var hbsObject = {
-			cats : result,
-			logged_in : req.session.logged_in,
-			username : req.session.username
-		}
-		res.render('cats/index', hbsObject);
-	})
+router.get('/cats', function(req, res) {
+  Cat.findAll({}).then(function(result) {
+    var hbsObject = {
+      cats: result,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    }
+    res.render('cats/index', hbsObject);
+  })
 });
 
-router.post('/cats/create', function(req,res) {
-	Cat.create({
-		name: req.body.name,
-		sleepy: req.body.sleepy,
-		user_id: req.session.user_id
-	}).then(function(newlyCreatedCat){
-		res.redirect('/cats')
-	});
-});
-
-router.put('/cats/update/:id', function(req,res) {
-	Cat.update({
-		sleepy: req.body.sleepy
-  },
-  {
-		where: { id : req.params.id }
-	}
-	).then(function (result) {
-		res.redirect('/cats');
-  }, function(rejectedPromiseError){
-		console.log(rejectedPromiseError);
+router.post('/cats/create', function(req, res) {
+  Cat.create({
+    name: req.body.name,
+    sleepy: req.body.sleepy,
+    user_id: req.session.user_id
+  }).then(function(newlyCreatedCat) {
+    res.redirect('/cats')
   });
 });
 
-router.delete('/cats/delete/:id', function(req,res) {
-	if (req.session.logged_in){
-		//check if cat belongs to user then let user delete cat
-		Cat.findAll({
-	    where: {$and: [{user_id: req.session.user_id}, {id: req.params.id}]}
-	  }).then(function(cats) {
-			if (cats.length > 0){
-				Cat.destroy({
-					where: { id : req.params.id }
-				}).then(function (result) {
-					res.redirect('/cats');
-				}, function(rejectedPromiseError){
-					console.log(rejectedPromiseError);
-				});
-			}else{
-				res.send("sorry you can't delete that cat.");
-			}
-		});
-	}else {
-		res.send("sorry you can't do that. You need to be logged in");
-	}
+router.put('/cats/update/:id', function(req, res) {
+  Cat.update({
+    sleepy: req.body.sleepy
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function(result) {
+    res.redirect('/cats');
+  }, function(rejectedPromiseError) {
+    console.log(rejectedPromiseError);
+  });
+});
+
+router.delete('/cats/delete/:id', function(req, res) {
+  if (req.session.logged_in) {
+    //check if cat belongs to user then let user delete cat
+    Cat.findAll({
+      where: {
+        $and: [{
+          user_id: req.session.user_id
+        }, {
+          id: req.params.id
+        }]
+      }
+    }).then(function(cats) {
+      if (cats.length > 0) {
+        Cat.destroy({
+          where: {
+            id: req.params.id
+          }
+        }).then(function(result) {
+          res.redirect('/cats');
+        }, function(rejectedPromiseError) {
+          console.log(rejectedPromiseError);
+        });
+      } else {
+        res.send("sorry you can't delete that cat.");
+      }
+    });
+  } else {
+    res.send("sorry you can't do that. You need to be logged in");
+  }
 
 });
 
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,91 +9,99 @@ var Cat = require('../models/models.js')[0];
 var User = require('../models/models.js')[1];
 
 //this is the users_controller.js file
-router.get('/users/new', function(req,res) {
-	res.render('users/new');
+router.get('/users/new', function(req, res) {
+  res.render('users/new');
 });
 
-router.get('/users/leaderboard', function(req,res) {
-	Cat.findAll({}).then(function(result) {
-		var hbsObject = {
-			cats: result,
-			logged_in : req.session.logged_in,
-			username: req.session.username
-		}
-		res.render('users/leaderboard', hbsObject);
-	})
+router.get('/users/leaderboard', function(req, res) {
+  Cat.findAll({}).then(function(result) {
+    var hbsObject = {
+      cats: result,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    }
+    res.render('users/leaderboard', hbsObject);
+  })
 });
 
 //change this so that it prints out past user bets
-router.get('/users/profile', function(req,res) {
-	Cat.findAll({}).then(function(result) {
-		var hbsObject = {
-			cats: result,
-			logged_in : req.session.logged_in,
-			username: req.session.username
-		}
-		res.render('users/profile', hbsObject)
-	})
+router.get('/users/profile', function(req, res) {
+  Cat.findAll({}).then(function(result) {
+    var hbsObject = {
+      cats: result,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    }
+    res.render('users/profile', hbsObject)
+  })
 });
 
-router.get('/users/sign-in', function(req,res) {
-	res.render('users/sign_in');
+router.get('/users/sign-in', function(req, res) {
+  res.render('users/sign_in');
 });
 
-router.get('/users/sign-out', function(req,res) {
+router.get('/users/sign-out', function(req, res) {
   req.session.destroy(function(err) {
-     res.redirect('/cats')
+    res.redirect('/users/leaderboard')
   })
 });
 
 //if user trys to sign in with the wrong password or email tell them that on the page
 router.post('/users/login', function(req, res) {
   User.findOne({
-    where: {email: req.body.email}
+    where: {
+      email: req.body.email
+    }
   }).then(function(user) {
     bcrypt.compare(req.body.password, user.password_hash, function(err, result) {
-        if (result == true){
-          //make session
+      if (result == true) {
+        //make session
 
-          req.session.logged_in = true;
-          req.session.user_id = user.id;
-          req.session.user_email = user.email;
-          req.session.username = user.username;
+        req.session.logged_in = true;
+        req.session.user_id = user.id;
+        req.session.user_email = user.email;
+        req.session.username = user.username;
 
-          res.redirect('/users/profile');
-        }
+        res.redirect('/users/profile');
+      }
     });
   })
 });
 
 //to do: check if the username/email exist - if they do then redirect the user back to the signup page and say sorry you need to choose a new name
-router.post('/users/create', function(req,res) {
-	User.findAll({
-    where: {$or: [{email: req.body.email}, {username: req.body.username}]}
+router.post('/users/create', function(req, res) {
+  User.findAll({
+    where: {
+      $or: [{
+        email: req.body.email
+      }, {
+        username: req.body.username
+      }]
+    }
   }).then(function(users) {
-		if (users.length > 0){
-			console.log(users)
-			res.send('we already have an email or username for this account')
-		}else{
-			bcrypt.genSalt(10, function(err, salt) {
-					bcrypt.hash(req.body.password, salt, function(err, hash) {
-						User.create({
-							username: req.body.username,
-							email: req.body.email,
-							password_hash: hash
-						}).then(function(user){
+    if (users.length > 0) {
+      console.log(users)
+      res.send('we already have an email or username for this account')
+    } else {
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+          User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password_hash: hash
+          }).then(function(user) {
 
-							req.session.logged_in = true;
-							req.session.user_id = user.id;
-							req.session.user_email = user.email;
-							req.session.username = user.username;
-							res.redirect('/cats')
-						});
-					});
-			});
+            req.session.logged_in = true;
+            req.session.user_id = user.id;
+            req.session.user_email = user.email;
+            req.session.username = user.username;
+            console.log('post users/create')
+          });
+        });
+      });
 
-		}
-	});
+    }
+  });
 });
 
 module.exports = router;
